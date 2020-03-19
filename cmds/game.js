d9fc66ac0cd36a41912809609033c24a99b6457b
gearysw <46032052+gearysw@users.noreply.github.com>
2020-03-18 23:45:31 -0700

    added support for multiple game rosters

--- cmds/game.js
+++ cmds/game.js
@@ -5,71 +5,78 @@ module.exports = {
     name: 'game',
     description: 'Add yourself to the list of people looking to join a game',
     execute: async (bot, message, args) => {
-        if (!args.length) {
-            fs.readFile('./gamers.json', (err, content) => {
-                if (err) console.error(err);
+        const games = ['csgo', 'squad', 'forza', 'civilisation', 'rocketleague', 'tabletop', 'apex', 'wreckfest', 'beatsaber', 'vr', 'factorio', 'minecraft', 'borderlands', 'halo', 'tarkov', 'siege', 'farming', 'overwatch'];
+
+        if (!games.includes(args[0])) {
+            bot.commands.get('help').execute(bot, message, args);
+        }
+        if (games.includes(args[0]) && args.length === 1) {
+            if (!fs.existsSync(`./games/${args[0]}.json`)) {
+                fs.writeFileSync(`./games/${args[0]}.json`, JSON.stringify({}));
+            }
+            fs.readFile(`./games/${args[0]}.json`, (err, content) => {
+                if (err) return console.error(err);
                 let gamers = JSON.parse(content);
 
                 const embed = new Discord.RichEmbed()
-                    .setTitle(`Ready players - ${Object.values(gamers).length}`)
+                    .setTitle(`${args[0]} roster - ${Object.values(gamers).length}`)
                     .setColor('#ff5555')
                     .setDescription(Object.values(gamers).join('\n'));
                 message.channel.send(embed);
             });
         }
-        if (args[0] === 'in') {
-            fs.readFile('./gamers.json', (err, content) => {
+        if (games.includes(args[0]) && args[1] === 'in') {
+            if (!fs.existsSync(`./games/${args[0]}.json`)) {
+                fs.writeFileSync(`./games/${args[0]}.json`, JSON.stringify({}));
+            }
+            fs.readFile(`./games/${args[0]}.json`, (err, content) => {
                 if (err) return console.error(err);
                 let gamers = JSON.parse(content);
 
-                if (Object.keys(gamers).indexOf(message.author.id) > -1) return message.channel.send(`You're already in the roster.`);
+                if (Object.keys(gamers).indexOf(message.author.id) > -1) return message.channel.send(`You're already in the ${args[0]} roster.`);
                 gamers[message.author.id] = (!message.member.nickname) ? message.author.username : message.member.nickname;
 
-                fs.writeFile('./gamers.json', JSON.stringify(gamers, null, '\t'), err => {
+                fs.writeFile(`./games/${args[0]}.json`, JSON.stringify(gamers, null, '\t'), err => {
                     if (err) return console.error(err);
                 });
 
                 const embed = new Discord.RichEmbed()
-                    .setTitle(`Ready players - ${Object.values(gamers).length}`)
+                    .setTitle(`${args[0]} roster - ${Object.values(gamers).length}`)
                     .setColor('#ff5555')
                     .setDescription(Object.values(gamers).join('\n'));
-                message.channel.send(`You're now on the roster.`, {
-                    embed: embed
-                });
+                message.channel.send(`You're now on the ${args[0]} roster.`, { embed: embed });
             });
         }
-        if (args[0] === 'out') {
-            fs.readFile('./gamers.json', (err, content) => {
+        if (games.includes(args[0]) && args[1] === 'out') {
+            if (!fs.existsSync(`./games/${args[0]}.json`)) {
+                fs.writeFileSync(`./games/${args[0]}.json`, JSON.stringify({}))
+            }
+            fs.readFile(`./games/${args[0]}.json`, (err, content) => {
                 if (err) return console.error(err);
                 let gamers = JSON.parse(content);
 
-                if (Object.keys(gamers).indexOf(message.author.id) === -1) return (message.channel.send(`You're not even on the roster. Why would you abandon something you haven't started?`));
+                if (Object.keys(gamers).indexOf(message.author.id) === -1) return message.channel.send(`You're not even on the roster. Why would you abandon something you haven't started?`);
                 delete gamers[message.author.id];
 
-                fs.writeFile('./gamers.json', JSON.stringify(gamers, null, '\t'), err => {
-                    if (err) return console.error(err);
+                fs.writeFile(`./games/${args[0]}.json`, JSON.stringify(gamers, null, '\t'), err => {
+                    if (err) console.error(err);
                 });
 
                 const embed = new Discord.RichEmbed()
-                    .setTitle(`Ready players - ${Object.values(gamers).length}`)
+                    .setTitle(`${args[0]} roster - ${Object.values(gamers).length}`)
                     .setColor('#ff5555')
                     .setDescription(Object.values(gamers).join('\n'));
-                message.channel.send(`You've abandoned your friends.`, {
-                    embed: embed
-                });
+                message.channel.send(`You've abandoned your friends.`, { embed: embed });
             });
         }
-        if (args[0] === 'clear') {
-            fs.writeFile('./gamers.json', JSON.stringify({}), err => {
-                if (err) console.error(err);
-                message.channel.send('Roster cleared.');
+        if (games.includes(args[0]) && args[1] === 'clear') {
+            fs.writeFile(`./games/${args[0]}.json`, JSON.stringify({}), err => {
+                if (err) return console.error(err);
+                message.channel.send(`${args[0]} roster cleared`);
             });
         }
-        if (args[0] === 'over') {
+        if (args[0] === 'over' || (games.includes(args[0]) && args[1] === 'over')) {
             message.channel.send('https://www.youtube.com/watch?v=IsS_VMzY10I');
-        }
-        if (args[0] === 'help') {
-            bot.commands.get('help').execute(bot, message, args);
         }
     }
 }
--- cmds/help.js
+++ cmds/help.js
@@ -4,12 +4,15 @@ module.exports = {
     name: 'help',
     description: 'Help!',
     execute: async (bot, message, args) => {
+        const games = ['csgo', 'squad', 'forza', 'civilisation', 'rocketleague', 'tabletop', 'apex', 'wreckfest', 'beatsaber', 'vr', 'factorio', 'minecraft', 'borderlands', 'halo', 'tarkov', 'siege', 'farming', 'overwatch'];
+
         const embed = new Discord.RichEmbed()
             .setTitle('Game Bot commands')
             .setColor('#ff5555')
-            .addField('!game in', 'Add yourself to the roster')
-            .addField('!game out', 'Remove yourself from the roster')
-            .addField('!game clear', 'Clear the roster')
+            .addField('!game [name of game] in', 'Add yourself to the roster')
+            .addField('!game [name of game] out', 'Remove yourself from the roster')
+            .addField('!game [name of game] clear', 'Clear the roster')
+            .addField('List of games', games.sort().join(', '));
 
         message.channel.send(embed);
     }
