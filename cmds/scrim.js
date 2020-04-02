04ef69125bbf5a7a995992870e7baf4300a273e6
gearysw <46032052+gearysw@users.noreply.github.com>
2020-04-02 11:23:20 -0700

    server spawn test with configs

--- cmds/scrim.js
+++ cmds/scrim.js
@@ -1,6 +1,6 @@
-// const { spawn } = require('child_process');
 const Discord = require('discord.js');
 const fs = require('fs');
+const { CSGO_PATH, connect } = require('../config.json');
 
 module.exports = {
     name: 'scrim',
@@ -94,59 +94,60 @@ module.exports = {
             child.stdin.write(`quit\n`);
         }
 
-        //         if (args[0] === 'start') {
-        //             if (!args[1]) return message.channel.send('Specify a map to play in.');
-        //             fs.readFile('./games/scrim.json', (err, content) => {
-        //                 if (err) return console.error(err);
-        //                 const roster = JSON.parse(content);
-        //                 if (Object.values(roster.team1).length != 5 && Object.values(roster.team2).length != 5) return message.channel.send('Not enough players for a scrim.');
+        if (args[0] === 'start') {
+            if (!args[1]) return message.channel.send('Specify a map to play in.');
+            fs.readFile('./games/scrim.json', (err, content) => {
+                if (err) return console.error(err);
+                const roster = JSON.parse(content);
+                // if (Object.values(roster.team1).length != 5 && Object.values(roster.team2).length != 5) return message.channel.send('Not enough players for a scrim.');
 
-        //                 const config = `
-        // "Match"
-        // {
-        //     "scrim" "1"
-        //     "side_type" "always_knife"
-        //     "players_per_team"  "5"
-        //     "num_maps"  "1"
-        //     "skip_veto" "1"
+                const config = `
+        "Match"
+        {
+            "scrim" "1"
+            "side_type" "always_knife"
+            "players_per_team"  "5"
+            "num_maps"  "1"
+            "skip_veto" "1"
 
-        //     "team1"
-        //     {
-        //         "name"  "Home"
-        //         "players"
-        //         {
-        //             "${Object.values(roster.team1)[0]}"   ""
-        //             "${Object.values(roster.team1)[1]}"   ""
-        //             "${Object.values(roster.team1)[2]}"   ""
-        //             "${Object.values(roster.team1)[3]}"   ""
-        //             "${Object.values(roster.team1)[4]}"   ""
-        //         }
-        //     }
+            "team1"
+            {
+                "name"  "Home"
+                "players"
+                {
+                    "${Object.values(roster.team1)[0]}"   ""
+                    "${Object.values(roster.team1)[1] || "\"STEAM_1:1:.....\""}"   ""
+                    "${Object.values(roster.team1)[2] || "\"STEAM_1:1:.....\""}"   ""
+                    "${Object.values(roster.team1)[3] || "\"STEAM_1:1:.....\""}"   ""
+                    "${Object.values(roster.team1)[4] || "\"STEAM_1:1:.....\""}"   ""
+                }
+            }
 
-        //     "cvars"
-        //     {
-        //         "get5_live_countdown_time"  "5"
-        //         "mp_halftime_duration"  "15"
-        //         "mp_match_can_clinch"   "1"
-        //         "mp_overtime_enable"    "1"
-        //         "mp_match_restart_delay"    "15"
-        //         "get5_max_pause_time"   "180"
-        //         "get5_check_auths"  "1"
-        //         "get5_demo_name_format" "ACRLscrim_{TIME}_{MAPNAME}"
-        //         "get5_kick_when_no_match_loaded"    "0"
-        //         "get5_print_damage" "1"
-        //     }
-        // }`;
+            "cvars"
+            {
+                "get5_live_countdown_time"  "5"
+                "mp_halftime_duration"  "15"
+                "mp_match_can_clinch"   "1"
+                "mp_overtime_enable"    "1"
+                "mp_match_restart_delay"    "15"
+                "get5_max_pause_time"   "180"
+                "get5_check_auths"  "1"
+                "get5_demo_name_format" "ACRLscrim_{TIME}_{MAPNAME}"
+                "get5_kick_when_no_match_loaded"    "0"
+                "get5_print_damage" "1"
+            }
+        }`;
 
-        //                 fs.writeFile(`${csgopath}\\scrim_template.cfg`, config, err => {
-        //                     if (err) return console.error(err);
-        //                 });
-        //             });
-        //             // console.log(args[1]);
-        //             const script = `cd /home/steam/steamcmd/csgo\n./srcds_run -game csgo -tickrate 128 +sv_setsteamaccount CFA746676095683147DB83EF35BFD586 -net_port_try 1 -console -usercon +game_type 0 +game_mode 1 +map ${args[1]} +maxplayers 12`;
-        //             fs.writeFile(`${csgopath}\\run.sh`, script, err => {
-        //                 if (err) return console.error(err);
-        //             });
-        // }
+                fs.writeFile(`${CSGO_PATH}/csgo/addons/sourcemod/configs/get5/scrim_template.cfg`, config, err => {
+                    if (err) return console.error(err);
+                    child.stdin.write(`./srcds_run -game csgo -tickrate 128 -net_port_try 1 -console -usercon +game_type 0 +game_mode 1 +map ${args[1]} +maxplayers 12\n`);
+                    const msg = await message.channel.send('Server spinning up...');
+                    bot.setTimeout(() => {
+                        msg.edit(`\`${connect}\``);
+                    }, 15000);
+                });
+            });
+            // console.log(args[1]);
+        }
     }
 }
