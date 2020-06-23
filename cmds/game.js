df67b1b4e6dcbd7fa219bfa9c92d84eecbdf2645
gearysw <46032052+gearysw@users.noreply.github.com>
2020-06-23 09:36:53 -0700

    added dice roll command

--- cmds/game.js
+++ cmds/game.js
@@ -7,74 +7,79 @@ module.exports = {
     execute: async (bot, message, args, child) => {
         const games = ['csgo', 'csgo10', 'squad', 'forza', 'civilisation', 'rocketleague', 'tabletop', 'apex', 'wreckfest', 'beatsaber', 'vr', 'factorio', 'minecraft', 'borderlands', 'halo', 'tarkov', 'siege', 'farming', 'overwatch', 'bf3'];
 
-        if (args[0] === 'over' || (games.includes(args[0]) && args[1] === 'over')) return message.channel.send('https://www.youtube.com/watch?v=IsS_VMzY10I');
-        if (!games.includes(args[0])) {
-            bot.commands.get('help').execute(bot, message, args);
-        }
-        if (games.includes(args[0]) && args.length === 1) {
-            if (!fs.existsSync(`./games/${args[0]}.json`)) {
-                fs.writeFileSync(`./games/${args[0]}.json`, JSON.stringify({}));
+        try {
+            if (args[0] === 'over' || (games.includes(args[0]) && args[1] === 'over')) return message.channel.send('https://www.youtube.com/watch?v=IsS_VMzY10I');
+            if (!games.includes(args[0])) {
+                bot.commands.get('help').execute(bot, message, args);
             }
-            fs.readFile(`./games/${args[0]}.json`, (err, content) => {
-                if (err) return console.error(err);
-                let gamers = JSON.parse(content);
-
-                const embed = new Discord.MessageEmbed()
-                    .setTitle(`${args[0]} roster - ${Object.values(gamers).length}`)
-                    .setColor('#ff5555')
-                    .setDescription(Object.values(gamers).join('\n'));
-                message.channel.send(embed);
-            });
-        }
-        if (games.includes(args[0]) && args[1] === 'in') {
-            if (!fs.existsSync(`./games/${args[0]}.json`)) {
-                fs.writeFileSync(`./games/${args[0]}.json`, JSON.stringify({}));
-            }
-            fs.readFile(`./games/${args[0]}.json`, (err, content) => {
-                if (err) return console.error(err);
-                let gamers = JSON.parse(content);
-
-                if (Object.keys(gamers).indexOf(message.author.id) > -1) return message.channel.send(`You're already in the ${args[0]} roster.`);
-                gamers[message.author.id] = (!message.member.nickname) ? message.author.username : message.member.nickname;
-
-                fs.writeFile(`./games/${args[0]}.json`, JSON.stringify(gamers, null, '\t'), err => {
+            if (games.includes(args[0]) && args.length === 1) {
+                if (!fs.existsSync(`./games/${args[0]}.json`)) {
+                    fs.writeFileSync(`./games/${args[0]}.json`, JSON.stringify({}));
+                }
+                fs.readFile(`./games/${args[0]}.json`, (err, content) => {
                     if (err) return console.error(err);
-                });
+                    let gamers = JSON.parse(content);
 
-                const embed = new Discord.MessageEmbed()
-                    .setTitle(`${args[0]} roster - ${Object.values(gamers).length}`)
-                    .setColor('#ff5555')
-                    .setDescription(Object.values(gamers).join('\n'));
-                message.channel.send(`You're now on the ${args[0]} roster.`, { embed: embed });
-            });
-        }
-        if (games.includes(args[0]) && args[1] === 'out') {
-            if (!fs.existsSync(`./games/${args[0]}.json`)) {
-                fs.writeFileSync(`./games/${args[0]}.json`, JSON.stringify({}))
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle(`${args[0]} roster - ${Object.values(gamers).length}`)
+                        .setColor('#ff5555')
+                        .setDescription(Object.values(gamers).join('\n'));
+                    message.channel.send(embed);
+                });
             }
-            fs.readFile(`./games/${args[0]}.json`, (err, content) => {
-                if (err) return console.error(err);
-                let gamers = JSON.parse(content);
+            if (games.includes(args[0]) && args[1] === 'in') {
+                if (!fs.existsSync(`./games/${args[0]}.json`)) {
+                    fs.writeFileSync(`./games/${args[0]}.json`, JSON.stringify({}));
+                }
+                fs.readFile(`./games/${args[0]}.json`, (err, content) => {
+                    if (err) return console.error(err);
+                    let gamers = JSON.parse(content);
 
-                if (Object.keys(gamers).indexOf(message.author.id) === -1) return message.channel.send(`You're not even on the roster. Why would you abandon something you haven't started?`);
-                delete gamers[message.author.id];
+                    if (Object.keys(gamers).indexOf(message.author.id) > -1) return message.channel.send(`You're already in the ${args[0]} roster.`);
+                    gamers[message.author.id] = (!message.member.nickname) ? message.author.username : message.member.nickname;
 
-                fs.writeFile(`./games/${args[0]}.json`, JSON.stringify(gamers, null, '\t'), err => {
-                    if (err) console.error(err);
+                    fs.writeFile(`./games/${args[0]}.json`, JSON.stringify(gamers, null, '\t'), err => {
+                        if (err) return console.error(err);
+                    });
+
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle(`${args[0]} roster - ${Object.values(gamers).length}`)
+                        .setColor('#ff5555')
+                        .setDescription(Object.values(gamers).join('\n'));
+                    message.channel.send(`You're now on the ${args[0]} roster.`, { embed: embed });
                 });
+            }
+            if (games.includes(args[0]) && args[1] === 'out') {
+                if (!fs.existsSync(`./games/${args[0]}.json`)) {
+                    fs.writeFileSync(`./games/${args[0]}.json`, JSON.stringify({}))
+                }
+                fs.readFile(`./games/${args[0]}.json`, (err, content) => {
+                    if (err) return console.error(err);
+                    let gamers = JSON.parse(content);
 
-                const embed = new Discord.MessageEmbed()
-                    .setTitle(`${args[0]} roster - ${Object.values(gamers).length}`)
-                    .setColor('#ff5555')
-                    .setDescription(Object.values(gamers).join('\n'));
-                message.channel.send(`You've abandoned your friends.`, { embed: embed });
-            });
+                    if (Object.keys(gamers).indexOf(message.author.id) === -1) return message.channel.send(`You're not even on the roster. Why would you abandon something you haven't started?`);
+                    delete gamers[message.author.id];
+
+                    fs.writeFile(`./games/${args[0]}.json`, JSON.stringify(gamers, null, '\t'), err => {
+                        if (err) console.error(err);
+                    });
+
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle(`${args[0]} roster - ${Object.values(gamers).length}`)
+                        .setColor('#ff5555')
+                        .setDescription(Object.values(gamers).join('\n'));
+                    message.channel.send(`You've abandoned your friends.`, { embed: embed });
+                });
+            }
+            if (games.includes(args[0]) && args[1] === 'clear') {
+                fs.writeFile(`./games/${args[0]}.json`, JSON.stringify({}), err => {
+                    if (err) return console.error(err);
+                    message.channel.send(`${args[0]} roster cleared`);
+                });
+            }
+        } catch (error) {
+            console.error(error);
         }
-        if (games.includes(args[0]) && args[1] === 'clear') {
-            fs.writeFile(`./games/${args[0]}.json`, JSON.stringify({}), err => {
-                if (err) return console.error(err);
-                message.channel.send(`${args[0]} roster cleared`);
-            });
-        }
+
     }
 }
---
+++ cmds/roll.js
@@ -0,0 +1,33 @@
+module.exports = {
+    name: 'roll',
+    aliases: ['rolldice', 'diceroll', 'dice'],
+    description: 'Roll XdY dice\nX = number of dice\nY = number of sides',
+    category: 'Miscellaneous',
+    showInHelp: true,
+    easteregg: false,
+    usage: 'XdY',
+    args: true,
+    execute: async (bot, message, args, child) => {
+        if (args[0].match(/\dd\d/)) {
+            let numDice = parseInt(args[0].split('d')[0]);
+            let diceType = parseInt(args[0].split('d')[1]);
+            let min = Math.ceil(numDice);
+            let max = Math.floor(numDice * diceType);
+            let num = Math.floor(Math.random() * (max - min + 1) + min);
+            if (num === 69) {
+                message.channel.send(`${num}...Nice`);
+            } else {
+                message.channel.send(num);
+            }
+        } else {
+            if (args[0].match(/d\d/)) {
+                let diceType = parseInt(args[0].split('d')[1]);
+                let num = Math.floor(Math.random() * diceType) + 1;
+                message.channel.send(`${num}\nBut please, follow the bloody syntax!`);
+
+            } else {
+                message.channel.send('Follow the syntax!');
+            }
+        }
+    }
+}
