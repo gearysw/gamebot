618360d7a57c9807ae6412147b6e08e494bca82c
gearysw <46032052+gearysw@users.noreply.github.com>
2020-09-21 00:54:31 -0700

    added random map choice

--- cmds/scrim.js
+++ cmds/scrim.js
@@ -149,13 +149,15 @@ module.exports = {
             if (!args[1]) return message.channel.send('Specify a map to play in. PLEASE PLEASE PLEASE write the map name correctly');
 
             // check for correct map name
-            const mapFiles = (await fs.promises.readdir(`${CSGO_PATH}/csgo/maps`)).filter(file => file.endsWith('.bsp'));
-            const maps = [];
-            for (const m of mapFiles) {
-                const map = m.slice(0, -4);
-                maps.push(map);
+            const activeMaps = ['de_anubis', 'de_cache', 'de_dust2', 'de_inferno', 'de_mirage', 'de_nuke', 'de_mutiny', 'de_overpass', 'de_swamp', 'de_vertigo'];
+            let map;
+            if (args[1] === 'random') {
+                map = activeMaps[Math.floor(Math.random() * activeMaps.length)];
+                message.channel.send(`Map selected: ${map}`);
+            } else {
+                if (!activeMaps.includes(args[1])) return message.channel.send('Map not found or not in active duty');
+                map = args[1];
             }
-            if (!maps.includes(args[1])) return message.channel.send('No map found with that name');
 
             fs.readFile('./games/scrim.json', (err, content) => {
                 if (err) return console.error(err);
@@ -168,7 +170,7 @@ module.exports = {
 
                 fs.writeFile(`${CSGO_PATH}/csgo/addons/sourcemod/configs/get5/scrim_template.cfg`, scrimRoster.config, async err => {
                     if (err) return console.error(err);
-                    child.stdin.write(`./srcds_run -game csgo -tickrate 128 -net_port_try 1 -console -usercon +game_type 0 +game_mode 1 +map ${args[1]} +maxplayers 12\n`);
+                    child.stdin.write(`./srcds_run -game csgo -tickrate 128 -net_port_try 1 -console -usercon +game_type 0 +game_mode 1 +map ${map} +maxplayers 12\n`);
                     const msg = await message.channel.send('Server spinning up...');
                     bot.setTimeout(() => {
                         msg.edit(`\`${connect}\``);
@@ -180,25 +182,34 @@ module.exports = {
         if (args[0] === 'test') {
             if (message.author.id != '197530293597372416') return;
 
-            const mapFiles = await fs.promises.readdir(`${CSGO_PATH}/csgo/maps`).filter(file => file.endsWith('.bsp'));
-            const maps = [];
-            for (const m of mapFiles) {
-                const map = m.slice(0, -4);
-                maps.push(map);
+            // const mapFiles = await fs.promises.readdir(`${CSGO_PATH}/csgo/maps`).filter(file => file.endsWith('.bsp'));
+            // const maps = [];
+            // for (const m of mapFiles) {
+            //     const map = m.slice(0, -4);
+            //     maps.push(map);
+            // }
+            // if (!maps.includes(args[1])) return message.channel.send('No map found with that name');
+
+            // const rosterArray = ['STEAM_0:1:52265309', 'STEAM_1:1:.....', 'STEAM_1:1:.....', 'STEAM_1:1:.....', 'STEAM_1:1:.....'];
+            // const testRoster = new Roster(rosterArray);
+
+            // fs.writeFile(`${CSGO_PATH}/csgo/addons/sourcemod/configs/get5/scrim_template.cfg`, testRoster.config, async err => {
+            //     if (err) return console.error(err);
+            //     child.stdin.write(`./srcds_run -debug -condebug -game csgo -tickrate 128 -net_port_try 1 -console -usercon +game_type 0 +game_mode 1 +map ${args[1]} +maxplayers 12\n`);
+            //     const msg = await message.channel.send('Spinning up test server');
+            //     bot.setTimeout(() => {
+            //         msg.edit(`\`${connect}\``);
+            //     }, 20000);
+            // });
+            const activeMaps = ['de_anubis', 'de_cache', 'de_dust2', 'de_inferno', 'de_mirage', 'de_nuke', 'de_mutiny', 'de_overpass', 'de_swamp', 'de_vertigo'];
+            let map;
+            if (args[1] === 'random') {
+                map = activeMaps[Math.floor(Math.random() * activeMaps.length)];
+                message.channel.send(`Map selected: ${map}`);
+            } else {
+                if (!activeMaps.includes(args[1])) return message.channel.send('Map not found or not in active duty');
+                map = args[1];
             }
-            if (!maps.includes(args[1])) return message.channel.send('No map found with that name');
-
-            const rosterArray = ['STEAM_0:1:52265309', 'STEAM_1:1:.....', 'STEAM_1:1:.....', 'STEAM_1:1:.....', 'STEAM_1:1:.....'];
-            const testRoster = new Roster(rosterArray);
-
-            fs.writeFile(`${CSGO_PATH}/csgo/addons/sourcemod/configs/get5/scrim_template.cfg`, testRoster.config, async err => {
-                if (err) return console.error(err);
-                child.stdin.write(`./srcds_run -debug -condebug -game csgo -tickrate 128 -net_port_try 1 -console -usercon +game_type 0 +game_mode 1 +map ${args[1]} +maxplayers 12\n`);
-                const msg = await message.channel.send('Spinning up test server');
-                bot.setTimeout(() => {
-                    msg.edit(`\`${connect}\``);
-                }, 20000);
-            });
         }
 
         if (args[0] === 'write') {
